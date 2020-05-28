b5bc86e39b83dd9e40f02f64a46377fd28949be7
Frogy <nurs.axe@gmail.com>
2020-05-28 16:27:08 +0600

    create column style
    column style

--- resources/views/blocks/contact_us/contact_us.blade.php
+++ resources/views/blocks/contact_us/contact_us.blade.php
@@ -1,1 +1,26 @@
-
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <p>
+                Get started with FitSmallBusiness’ #1 choice for online bookkeeping.
+            </p>
+            <p>
+                Try the best—free. Sign up for a free trial and get full access to Bench.
+                We’ll do a month of your bookkeeping in one business day, and deliver a set of financial statements for you to keep.
+            </p>
+            <form action="">
+                <ul>
+                    <li>
+                        <input type="text" placeholder="Full Name">
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Full Name">
+                    </li>
+                    <li>
+                        <button></button>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</div>
