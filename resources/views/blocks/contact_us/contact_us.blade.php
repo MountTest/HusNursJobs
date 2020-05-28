a96049f6f798fc9d685fd72d70ed2490b2f0673f
Frogy <nurs.axe@gmail.com>
2020-05-28 17:25:36 +0600

    style animate
    asdasd

--- resources/views/blocks/contact_us/contact_us.blade.php
+++ resources/views/blocks/contact_us/contact_us.blade.php
@@ -2,23 +2,24 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-6">
-                <p class="contact-us-text tracking-in-expand">
+                <p class="contact-us-text" id="header-text">
                     Get started with FitSmallBusiness’ #1 choice for online bookkeeping.
                 </p>
-                <p>
+                <p class="">
                     Try the best—free. Sign up for a free trial and get full access to Bench.
                     We’ll do a month of your bookkeeping in one business day, and deliver a set of financial statements for you to keep.
                 </p>
+                <button class="btn btn-primary btn-text">Touch</button>
                 <form class="contact-us-form" action="">
                     <ul>
                         <li>
-                            <input type="text" placeholder="Full Name">
+                            <input type="text wobble-hor-bottom" placeholder="Full Name">
                         </li>
                         <li>
-                            <input type="text" placeholder="Full Name">
+                            <input type="text " placeholder="Full Name">
                         </li>
                         <li>
-                            <button class="btn btn-primary">Contact US</button>
+                            <button class="btn btn-primary ">Contact US</button>
                         </li>
                     </ul>
                 </form>
@@ -32,7 +33,10 @@
 
 @push('scripts')
     <script>
-
+        $('.btn-text').on('click', function (e) {
+            let btn = e.currentTarget();
+            console.log(btn);
+        })
     </script>
 @endpush
 
