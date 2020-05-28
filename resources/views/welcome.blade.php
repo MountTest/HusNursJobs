d1f4cfc5cedd4b1ad1150775549bcba8ce0f8ffd
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-05-28 15:58:32 +0600

    add code to header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 
-        @include('blocks.header.header')
-        @include('blocks.main.main_first_block')
-        @include('blocks.tabs.main_tabs')
-        @include('blocks.advantages.advatages')
-        @include('blocks.prices.prices')
-        @include('blocks.contact_us.contact_us')
-        @include('blocks.footer.footer')
-
+    @include('blocks.header.header')
+    @include('blocks.main.main_first_block')
+    @include('blocks.tabs.main_tabs')
+    @include('blocks.advantages.advatages')
+    @include('blocks.prices.prices')
+    @include('blocks.contact_us.contact_us')
+    @include('blocks.footer.footer')
 
 @endsection
 
