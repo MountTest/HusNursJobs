24690125db6e981a91d08debd45451a7514bf39c
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-05-28 16:13:59 +0600

    include main

--- resources/views/blocks/header/header.blade.php
+++ resources/views/blocks/header/header.blade.php
@@ -4,18 +4,27 @@
             <img src="http://flexbox.ninja/assets/images/logo.svg" width="170" height="95" alt="Flexbox.ninja">
         </h1>
         <nav class="main-nav">
-            <ul class="main-nav-list">
-                <li>
-                    <a href="#">Home</a>
+            <ul class="main-nav-list text-dark">
+                <li class="text-dark">
+                    <a href="#">Main</a>
                 </li>
                 <li>
-                    <a href="#">About us</a>
+                    <a href="#">Tabs</a>
                 </li>
                 <li>
-                    <a href="#">Our clients</a>
+                    <a href="#">Advantages</a>
                 </li>
                 <li>
-                    <a href="#">Contact</a>
+                    <a href="#">Prices</a>
+                </li>
+                <li>
+                    <a href="#">Contact Us</a>
+                </li>
+                <li>
+                    <a href="#">Sing In</a>
+                </li>
+                <li>
+                    <a href="#">Sing Up</a>
                 </li>
             </ul>
         </nav>
