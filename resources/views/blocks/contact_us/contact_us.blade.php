52409cef817ce2688a0c4fde26e557ef7764068a
Frogy <nurs.axe@gmail.com>
2020-05-28 16:53:07 +0600

    style add

--- resources/views/blocks/contact_us/contact_us.blade.php
+++ resources/views/blocks/contact_us/contact_us.blade.php
@@ -1,26 +1,32 @@
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            <p>
-                Get started with FitSmallBusiness’ #1 choice for online bookkeeping.
-            </p>
-            <p>
-                Try the best—free. Sign up for a free trial and get full access to Bench.
-                We’ll do a month of your bookkeeping in one business day, and deliver a set of financial statements for you to keep.
-            </p>
-            <form class="contact-us-form" action="">
-                <ul>
-                    <li>
-                        <input type="text" placeholder="Full Name">
-                    </li>
-                    <li>
-                        <input type="text" placeholder="Full Name">
-                    </li>
-                    <li>
-                        <button class="btn btn-primary">Contact US</button>
-                    </li>
-                </ul>
-            </form>
+<section class="contact-us-bg">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <p class="contact-us-text">
+                    Get started with FitSmallBusiness’ #1 choice for online bookkeeping.
+                </p>
+                <p>
+                    Try the best—free. Sign up for a free trial and get full access to Bench.
+                    We’ll do a month of your bookkeeping in one business day, and deliver a set of financial statements for you to keep.
+                </p>
+                <form class="contact-us-form" action="">
+                    <ul>
+                        <li>
+                            <input type="text" placeholder="Full Name">
+                        </li>
+                        <li>
+                            <input type="text" placeholder="Full Name">
+                        </li>
+                        <li>
+                            <button class="btn btn-primary">Contact US</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <div class="col-5 text-center">
+                <img src="https://images.ctfassets.net/88a6qmzs2wdz/5Fdi0iNZ4zeaFh9RAAn9kk/500927abde48b7c081bcbc304f22992e/Yellow-CTA-B.jpg?h=228&q=90&w=370" alt="" class="img-fluid">
+            </div>
         </div>
     </div>
-</div>
+</section>
+
