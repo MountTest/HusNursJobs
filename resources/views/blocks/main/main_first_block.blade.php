6cf7ebd14ad888ebe6c970a571302862bdf69f98
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-05-28 17:36:26 +0600

    add animate

--- resources/views/blocks/main/main_first_block.blade.php
+++ resources/views/blocks/main/main_first_block.blade.php
@@ -2,12 +2,12 @@
     <div class="row">
         <div class="col-5" style="padding-top: 10%">
             <h1 class="h1 text-pop-up-top" style="margin-top: 0;margin-bottom: 0;">Create, your way</h1><br>
-            <h2 class="grey " style="margin-top: 0;color: #808080">Part spreadsheet, part database, and <br>entirely flexible, teams use Airtable to <br> organize their work, their way.</h2>
+            <h2 class="grey " id="text-h2" style="margin-top: 0;color: #808080">Part spreadsheet, part database, and <br>entirely flexible, teams use Airtable to <br> organize their work, their way.</h2>
             <input type="text" size="30" placeholder="Enter your email address" style="padding: 15px;border-color: #e9e9e9;border-radius: 10px">
             <button  class="get-started "  style="padding: 15px;color: white;background-color: #2d7ff9;border-radius: 10px;" >Get Started </button>
         </div>
         <div class="col-6">
-            <img src="{{ asset('image/shapes_collaboration.png') }}" alt="main" class="img-fluid">
+            <img id="img-hover" src="{{ asset('image/shapes_collaboration.png') }}" alt="main" class="img-fluid">
         </div>
     </div>
 </div>
@@ -39,11 +39,12 @@
 <script>
     $('.get-started').click(function (e) {
         let btn = $(e.currentTarget);
-        console.log(btn);
 
-        if(btn.hasClass('text-pop-up-top') == false){
-            $('.get-started').removeClass('text-pop-up-top');
-            btn.addClass('text-pop-up-top');
+        if($('#text-h2').hasClass('text-pop-up-top') == false){
+            $('#text-h2').removeClass('text-pop-up-top');
+            $('#text-h2').addClass('text-pop-up-top');
+            $('#img-hover').addClass('text-pop-up-top');
+            $('.get-started').addClass('jello-horizontal');
         }
     });
 </script>
